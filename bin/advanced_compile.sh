2b5b035ab3e60c07bbdd289d7c6ab2c9c867ea3f
Richard Anaya <richard.anaya@gmail.com>
2011-08-18 18:28:13 -0500

    adding pretty advanced compile

--- bin/advanced_compile.sh
+++ bin/advanced_compile.sh
@@ -1,2 +1,2 @@
-python closure-tools/closure/bin/build/closurebuilder.py --root closure-tools --root ../src --namespace "AppLayout" --output_mode=compiled --compiler_jar=closure-tools/compiler.jar --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" --compiler_flags="--formatting=PRETTY_PRINT" > output/applayout.min.js
+python closure-tools/closure/bin/build/closurebuilder.py --root closure-tools --root ../src --namespace "AppLayout" --output_mode=compiled --compiler_jar=closure-tools/compiler.jar --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" > output/applayout.min.js
 ./move_outputs.sh
